169fc2f2f46458197c4bda46c604782bfe80ab2a
wesleyara <wes1gom@gmail.com>
2023-03-24 01:04:55 -0300

    fix: change variable name

--- 002-variables/script.sh
+++ 002-variables/script.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-NUMERO01=20
-NUMERO02=12
+NUMBER01=20
+NUMBER02=12
 
 # for execute a mathematical operation, we must use $(())
-TOTAL=$((NUMERO01+NUMERO02))
+RESULT=$((NUMBER01+NUMBER02))
 
-echo $TOTAL
+echo $RESULT
 
 # for execute a command, we must use $()
 COMMAND=$(ls -a)
